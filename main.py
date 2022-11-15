7a5d2459f832c223f7b7d759a65467e1a8dea002
LAPTOP-MVV820R8\timst <Tim.Stengelin@htwg-konstanz.de>
2022-11-15 21:58:31 +0900

    minor changes

--- main.py
+++ main.py
@@ -1,5 +1,15 @@
-print('TODO')
+####################################################################################################
+# main.py
+####################################################################################################
 
-# input
-# if setupModel
-# else recordDataset
+from setupModel.setupModel import setupModel
+from recordDataset.recordDataset import recordDataset
+
+task = ' '
+while task not in 'srx':
+    task = input('press key:\n(s) for setup model\n(r) for record dataset\n(x) for exit\n').lower()
+
+if task == 's':
+    setupModel()
+elif task == 'r':
+    recordDataset()
