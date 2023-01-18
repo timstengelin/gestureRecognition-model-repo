20ef119640cf0c5eaeabcd2e2c317c00d3e03cde
LAPTOP-MVV820R8\timst <Tim.Stengelin@htwg-konstanz.de>
2023-01-18 10:23:33 +0100

    minor changes

--- setupModel/setupFinalModel.py
+++ setupModel/setupFinalModel.py
@@ -1,1 +1,41 @@
-# TODO
+####################################################################################################
+# setupFinalModel.py
+####################################################################################################
+
+import numpy as np
+from keras import models
+from keras import layers
+from loadDataset import loadDataset
+
+
+def setupFinalModel():
+    # ******************** LOAD DATASET ********************
+    data_train, labels_train, data_validation, labels_validation, data_test, labels_test = loadDataset()
+
+    # ******************** DEFINE MODEL ********************
+    model = models.Sequential()
+    model.add(layers.Dense(512, activation='relu', input_shape=(1506,)))
+    model.add(layers.Dropout(0.25))
+    model.add(layers.Dense(512, activation='relu'))
+    model.add(layers.Dropout(0.25))
+    model.add(layers.Dense(10, activation='softmax'))
+
+    # ******************** COMPILE MODEL ********************
+    model.compile(optimizer='rmsprop',
+                  loss='categorical_crossentropy',
+                  metrics=['accuracy'])
+
+    # ******************** TRAIN MODEL ********************
+    history = model.fit(np.concatenate((data_train, data_validation), axis=0),
+                        np.concatenate((labels_train, labels_validation), axis=0),
+                        epochs=10,
+                        batch_size=8)
+
+    # ******************** EVALUATE MODEL ********************
+    results = model.evaluate(data_test, labels_test)
+    print("test loss: {0}, test accuarcy: {1}".format(results[0], results[1]))
+
+    # ******************** SHOW MODEL SUMMARY ********************
+    model.summary()
+
+    return model
--- setupModel/setupParameterizationModel.py
+++ setupModel/setupParameterizationModel.py
@@ -1,39 +1,16 @@
 ####################################################################################################
-# setupModel.py
+# setupParameterizationModel.py
 ####################################################################################################
 
 import matplotlib.pyplot as plt
 from keras import models
 from keras import layers
 from loadDataset import loadDataset
-from sklearn.model_selection import StratifiedShuffleSplit
-from keras.utils.np_utils import to_categorical
 
 
 def setupParameterizationModel():
     # ******************** LOAD DATASET ********************
-    data, labels = loadDataset()
-
-    # ******************** SPLIT DATA ********************
-    trainTestSplit = StratifiedShuffleSplit(n_splits = 1, test_size = 0.25)
-    trainValidationSplit =  StratifiedShuffleSplit(n_splits = 1, test_size = 0.25)
-
-    for i, (idx_train, idx_test) in enumerate(trainTestSplit.split(data, labels)):
-        data_train, data_test = data[idx_train], data[idx_test]
-        labels_train, labels_test = labels[idx_train], labels[idx_test]
-    print("trainTestSplit:\nshape \"data_train\": {0}\nshape \"labels_train\": {1}\nshape \"data_test\": {2}\nshape \"labels_test\": {3}\n" \
-          .format(data_train.shape, labels_train.shape, data_test.shape, labels_test.shape))
-
-    for i, (idx_train, idx_validation) in enumerate(trainTestSplit.split(data_train, labels_train)):
-        data_train, data_validation = data_train[idx_train], data_train[idx_validation]
-        labels_train, labels_validation = labels_train[idx_train], labels_train[idx_validation]
-    print("trainValidationSplit:\nshape \"data_train\": {0}\nshape \"labels_train\": {1}\nshape \"data_validation\": {2}\nshape \"labels_validation\": {3}\n" \
-          .format(data_train.shape, labels_train.shape, data_validation.shape, labels_validation.shape))
-
-    # ******************** ONE-HOT ENCODE LABELS ********************
-    labels_train = to_categorical(labels_train)
-    labels_validation = to_categorical(labels_validation)
-    labels_test = to_categorical(labels_test)
+    data_train, labels_train, data_validation, labels_validation, data_test, labels_test = loadDataset()
 
     # ******************** DEFINE MODEL ********************
     model = models.Sequential()
@@ -86,9 +63,7 @@ def setupParameterizationModel():
 
     # ******************** EVALUATE MODEL ********************
     results = model.evaluate(data_test, labels_test)
-    print(results)
+    print("test loss: {0}, test accuarcy: {1}".format(results[0], results[1]))
 
     # ******************** SHOW PLOTS ********************
     plt.show()
-
-setupParameterizationModel()
